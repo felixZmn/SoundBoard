4dc59e0811a03c99bc7d1e3ba1251cdf1177b22e
felixZmn <felix.zimmermann.1.de@gmail.com>
2023-11-29 13:19:50 +0100

    Added version info text

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 33
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -37,6 +37,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
@@ -49,7 +50,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.1")
--- app/src/main/java/com/nasenbaer/soundboard/ui/ButtonsScreen.kt
+++ app/src/main/java/com/nasenbaer/soundboard/ui/ButtonsScreen.kt
@@ -1,10 +1,18 @@
 package com.nasenbaer.soundboard.ui
 
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.nasenbaer.soundboard.BuildConfig
 
 @Composable
 fun ButtonsScreen(contents: Map<String, () -> Unit>) {
@@ -14,6 +22,9 @@ fun ButtonsScreen(contents: Map<String, () -> Unit>) {
         contents.forEach {
             ButtonCard(name = it.key, onClick = it.value)
         }
+    }
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Bottom) {
+        Text(text = "Version: " + BuildConfig.VERSION_NAME, color = Color.Black.copy(alpha = 0.5f))
     }
 }
 
